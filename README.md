687a11a093b2ff248457eff4540777f3d9758a47
Paul Estipona <paul_estipona@yahoo.com>
2023-01-07 10:43:37 -0500

    Edited ReadME in GitHub

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Real Time Bus Tracking:
+
+![](ScreenShots/01_ScreenShot.png)
+
+* a navbar with drop down menus for options
+* make the marker icon rotate to the direction of travel
+* menu to switch between different bus icons
+* menu to switch between different types of maps
+* menu to switch between light and dark mode
+* improve starting location of marker
+* side-panel that shows the route in text with icons in real-time
--- mapanimation.js
+++ mapanimation.js
@@ -1,41 +1,57 @@
 // import mapbox public api token
 mapboxgl.accessToken = 'pk.eyJ1IjoicGVzdGlwb25hIiwiYSI6ImNsY2pvd2Q0aDZsMTIzbnBqbDE3Y25xdzEifQ.SJcJUIBw9rtvGfkroUy7Cw';
 
-// create map with style, coordinates, and zoom
+// create a map with style, coordinates, and zoom
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/streets-v12',
     center: [-71.104081, 42.365554],
     zoom: 14
 });
 
-// create a new marker
-var marker = new mapboxgl.Marker()
+// Get Real-Time Bus Data
+async function getBusLocations(){
+    const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+    const response = await fetch(url);
+    const json = await response.json();
+    return json.data;
+}
+
+async function getBusCoordinates(){
+    // Get Real-time Bus Location
+    const locations = await getBusLocations();
+    let coordinates = {
+        Lng: locations[0].attributes.longitude,
+        Lat: locations[0].attributes.latitude
+    };
+    return coordinates;
+}
+
+// create a custom icon for the Marker
+var icon = document.createElement('div');
+icon.className = 'marker';
+
+// Create and add the custom Marker to the Map
+var marker = new mapboxgl.Marker(icon)
     .setLngLat([-71.092761, 42.357575])
     .addTo(map);
 
-// array of bus stops
-const busStops = [
-    [-71.093729, 42.359244],
-    [-71.094915, 42.360175],
-    [-71.095800, 42.360698],
-    [-71.099558, 42.362953],
-    [-71.103476, 42.365248],
-    [-71.106067, 42.366806],
-    [-71.108717, 42.368355],
-    [-71.110799, 42.369192],
-    [-71.113095, 42.370218],
-    [-71.115476, 42.372085],
-    [-71.117585, 42.373016],
-    [-71.118625, 42.374863]
-];
 
-var counter = 0;
-function move() {
+// Update Marker on Real Time
+async function move() {
+    // Get Real-time Bus Location
+    const coordinates = await getBusCoordinates();
+    let Lng = coordinates.Lng;
+    let Lat = coordinates.Lat;
+
+    // For Tracking & Debugging
+    console.log(new Date());
+    console.log(Lng);
+    console.log(Lat);
+
+    // Move Marker based on new location
     setTimeout(() => {
-        if (counter >= busStops.length) return;
-        marker.setLngLat(busStops[counter]);
-        counter++;
+        marker.setLngLat([Lng, Lat]);
         move();
-    }, 1000);
+    }, 10000);
 }
